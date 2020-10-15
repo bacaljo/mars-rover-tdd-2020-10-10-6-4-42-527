f9fbd83e1593f92c92723096e9014cd6a091529a
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 17:23:35 +0800

    [Mars Rover] [John Emmanuel] test: create a test for instructRover to change direction to W if direction is S and instruction is R

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -6,6 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
 
+    private final char NORTH = 'N';
+    private final char WEST = 'W';
+    private final char SOUTH = 'S';
+    private final char EAST = 'E';
+
     @Test
     void should_return_x_0_y_1_d_N_when_Mars_Rover_instruct_rover_given_x_0_y_0_d_N_and_M() {
         // given
@@ -74,5 +79,19 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getX());
         assertEquals(0, marsRover.getY());
         assertEquals('E', marsRover.getDirection());
+    }
+
+    @Test
+    void should_return_x_0_y_0_d_W_when_Mars_Rover_instruct_rover_given_x_0_y_0_d_S_and_R() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, SOUTH);
+
+        // when
+        marsRover.instructRover("R");
+
+        // then
+        assertEquals(0, marsRover.getX());
+        assertEquals(0, marsRover.getY());
+        assertEquals(WEST, marsRover.getDirection());
     }
 }

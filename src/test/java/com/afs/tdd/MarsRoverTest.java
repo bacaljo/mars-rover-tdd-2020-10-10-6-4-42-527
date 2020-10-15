af172e83ad4bf72909e07dd479287c600d6c140c
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 17:10:40 +0800

    [Mars Rover] [John Emmanuel] test: create a test for instructRover to change direction to W if direction is N and instruction is L

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -19,4 +19,18 @@ class MarsRoverTest {
         assertEquals(1, marsRover.getY());
         assertEquals('N', marsRover.getDirection());
     }
+
+    @Test
+    void should_return_x_0_y_0_d_W_when_Mars_Rover_instruct_rover_given_x_0_y_0_d_N_and_L() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'N');
+
+        // when
+        marsRover.instructRover("L");
+
+        // then
+        assertEquals(0, marsRover.getX());
+        assertEquals(0, marsRover.getY());
+        assertEquals('W', marsRover.getDirection());
+    }
 }

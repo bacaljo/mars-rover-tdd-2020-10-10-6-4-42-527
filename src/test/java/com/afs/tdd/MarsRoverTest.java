8c3018fb83138f570f7d52b7d0898a702dfda5b6
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 17:16:21 +0800

    [Mars Rover] [John Emmanuel] test: create a test for instructRover to change y value to 5 if initial y is 6 and direction is S and instruction is M

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -47,4 +47,18 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getY());
         assertEquals('E', marsRover.getDirection());
     }
+
+    @Test
+    void should_return_x_0_y_5_d_S_when_Mars_Rover_instruct_rover_given_x_0_y_6_d_S_and_M() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 6, 'S');
+
+        // when
+        marsRover.instructRover("M");
+
+        // then
+        assertEquals(0, marsRover.getX());
+        assertEquals(5, marsRover.getY());
+        assertEquals('S', marsRover.getDirection());
+    }
 }

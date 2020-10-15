2bdaa182e996a7d0d97e6f6e89641ec2bfa5bceb
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 22:31:29 +0800

    [Mars Rover] [John Emmanuel] refactor: implement command pattern by extracting MarsRover commanding logic to RoverCommandExecutor

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -2,19 +2,10 @@ package com.afs.tdd;
 
 import com.afs.tdd.property.Direction;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class MarsRover {
     private int x;
     private int y;
     private Direction direction;
-
-    private final String MOVE = "M";
-    private final String TURN_LEFT = "L";
-    private final String TURN_RIGHT = "R";
-
-    private final String EMPTY_STRING = "";
 
     public MarsRover(int x, int y, Direction direction) {
         this.x = x;
@@ -22,45 +13,7 @@ public class MarsRover {
         this.direction = direction;
     }
 
-    public void commandRover(String commands) throws CommandNotDefinedException {
-        validateCommands(commands);
-        List<String> commandList = parseCommands(commands);
-        performCommands(commandList);
-    }
-
-    private void performCommands(List<String> commandList) {
-        for (String command : commandList) {
-            switch (command) {
-                case MOVE:
-                    move();
-                    break;
-                case TURN_LEFT:
-                    turnLeft();
-                    break;
-                case TURN_RIGHT:
-                    turnRight();
-                    break;
-            }
-        }
-    }
-
-    private List<String> parseCommands(String commands) {
-        return Arrays.asList(commands.split(EMPTY_STRING));
-    }
-
-    private void validateCommands(String commands) throws CommandNotDefinedException {
-        boolean isValid = commands.replace(MOVE, EMPTY_STRING)
-                .replace(TURN_LEFT, EMPTY_STRING)
-                .replace(TURN_RIGHT, EMPTY_STRING)
-                .trim()
-                .isEmpty();
-        if (!isValid) {
-            throw new CommandNotDefinedException(String.format("Commands contained unsupported values. Only the following can be used: %s, %s and %s",
-                    MOVE, TURN_LEFT, TURN_RIGHT));
-        }
-    }
-
-    private void turnRight() {
+    public void turnRight() {
         int currentIndex = direction.ordinal();
         int lastIndex = Direction.values().length - 1;
 
@@ -69,7 +22,7 @@ public class MarsRover {
                 : Direction.values()[currentIndex + 1];
     }
 
-    private void turnLeft() {
+    public void turnLeft() {
         int currentIndex = direction.ordinal();
         int lastIndex = Direction.values().length - 1;
 
@@ -78,7 +31,7 @@ public class MarsRover {
                 : Direction.values()[currentIndex - 1];
     }
 
-    private void move() {
+    public void move() {
         switch (direction) {
             case NORTH:
                 y++;
--- src/main/java/com/afs/tdd/RoverCommandExecutor.java
+++ src/main/java/com/afs/tdd/RoverCommandExecutor.java
@@ -1,12 +1,51 @@
 package com.afs.tdd;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RoverCommandExecutor {
 
-    public RoverCommandExecutor(MarsRover marsRover) {
+    private final Map<String, Command> stringCommandMap;
 
+    private final String MOVE = "M";
+    private final String TURN_LEFT = "L";
+    private final String TURN_RIGHT = "R";
+
+    private final String EMPTY_STRING = "";
+
+    public RoverCommandExecutor(MarsRover marsRover) {
+        stringCommandMap = new HashMap<>();
+        stringCommandMap.put(MOVE, marsRover::move);
+        stringCommandMap.put(TURN_LEFT, marsRover::turnLeft);
+        stringCommandMap.put(TURN_RIGHT, marsRover::turnRight);
     }
 
     public void commandRover(String commands) throws CommandNotDefinedException {
+        validateCommands(commands);
+        List<String> commandList = parseCommands(commands);
+        performCommands(commandList);
+    }
 
+    private void performCommands(List<String> commandList) {
+        commandList.forEach(command -> stringCommandMap.get(command).execute());
+    }
+
+    private List<String> parseCommands(String commands) {
+        return Arrays.asList(commands.split(EMPTY_STRING));
+    }
+
+    private void validateCommands(String commands) throws CommandNotDefinedException {
+        boolean isValid = commands.replace(MOVE, EMPTY_STRING)
+                .replace(TURN_LEFT, EMPTY_STRING)
+                .replace(TURN_RIGHT, EMPTY_STRING)
+                .trim()
+                .isEmpty();
+
+        if (!isValid) {
+            throw new CommandNotDefinedException(String.format("Commands contained unsupported values. Only the following can be used: %s, %s and %s",
+                    MOVE, TURN_LEFT, TURN_RIGHT));
+        }
     }
 }

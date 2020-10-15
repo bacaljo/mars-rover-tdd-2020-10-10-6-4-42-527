4d2afd76fc1b29948ff5fec135646e79ae55c12a
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-15 17:06:56 +0800

    [Mars Rover] [John Emmanuel] test: create a MarsRover class with x, y and direction as attributes with getters and instructRover and move methods; create a test for instructRover to change y value to 1 if initial y is 1 and direction is N

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,29 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int x;
+    private int y;
+    private char direction;
+
+    public MarsRover(int x, int y, char direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public void instructRover(String instructions) {
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+}
---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,22 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MarsRoverTest {
+
+    @Test
+    void should_return_x_0_y_1_d_N_when_Mars_Rover_instruct_rover_given_x_0_y_0_d_N_and_M() {
+        // given
+        MarsRover marsRover = new MarsRover(0, 0, 'N');
+
+        // when
+        marsRover.instructRover("M");
+
+        // then
+        assertEquals(0, marsRover.getX());
+        assertEquals(1, marsRover.getY());
+        assertEquals('N', marsRover.getDirection());
+    }
+}
